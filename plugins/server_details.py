c4f0318bb1a8ed9480fcb9abcba8a804a49c08c2
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-12-11 01:22:30 +0530

    Update server_details.py

--- plugins/server_details.py
+++ plugins/server_details.py
@@ -25,11 +25,6 @@ bot_start_time = time.time()
 @Client.on_message(filters.command(["server"]), group=-2)
 async def start(client, message):
     bot_uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time)) 
-    joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("JOIN", url="https://t.me/TGBotsCollection")],
-        [InlineKeyboardButton(
-            "Try", url="https://t.me/TGBotsCollectionbot")]
-    ])
     welcomed = f"<b>--Server Details--</b>\n<b>CPU:</b> {psutil.cpu_percent()}%\n<b>RAM:</b> {psutil.virtual_memory().percent}%\n<b>DISK:</b> {psutil.disk_usage('/').percent}%\n\n <b><i>Bot Uptime :</i></b> {bot_uptime}"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
