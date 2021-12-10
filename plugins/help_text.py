75cfc91bbdbd7ab40d55b0306c72289b73e19f81
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-12-11 01:25:09 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -60,9 +60,9 @@ async def get_me_info(bot, update):
 @pyrogram.Client.on_message(pyrogram.filters.command(["starting"]))
 async def start(bot, update):
     # logger.info(update)
-    await update.reply(f"<b>Hii {update.chat.first_name}!</b>\nThis is a Telegram Multipurpose Bot Which can do many functions. /help for more details...  ",reply_markup=InlineKeyboardMarkup(
+    await update.reply(f"<b>Hii {update.chat.first_name}!</b>\nThis is a all in one Bot Which can do many functions. /help for more details...  ",reply_markup=InlineKeyboardMarkup(
             [
-                    InlineKeyboardButton('JOIN', url='https://t.me/TGBotsCollection')
+                    InlineKeyboardButton('JOIN', url='https://t.me/pyrogrammers')
                 ]
         )
     )
